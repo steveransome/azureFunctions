1915c0a06581696060d0bb8d6144d3e7b6c86747
Steve Ransome <sransome@creativesoftware.com.au>
2020-05-08 19:42:30 +1000

    added helper class

---
+++ FunctionAppHelper.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Company.Helper
+{
+    public static class FunctionAppHelper
+    {
+        public static string SteveTest(string code)
+        {          
+            return code + "_ransome";
+        }
+    }
+
+}
