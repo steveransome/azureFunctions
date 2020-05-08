25b42ec131cab9cdb052c1925a729bba775dbdb6
Steve Ransome <sransome@creativesoftware.com.au>
2020-05-08 19:42:07 +1000

    added helper class

--- HttpExample.cs
+++ HttpExample.cs
@@ -7,16 +7,13 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Company.Helper;
 
 namespace Company.Function
 {
     public static class HttpExample
     {
-        public static string KelTest(string code)
-        {          
-            return code + "_kelly";
-        }
-
+        
         [FunctionName("HttpExample")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
@@ -29,7 +26,7 @@ namespace Company.Function
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
-            name = KelTest(name);
+            name = FunctionAppHelper.SteveTest(name);
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
